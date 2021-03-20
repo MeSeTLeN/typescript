a39b60258b6cbae0d683b116aad11642bbf088d8
DeAdFrOnt <semenukmaks@gmail.com>
2021-03-20 13:58:18 +0200

    class

--- TypeScript/interfaces.ts
+++ TypeScript/interfaces.ts
@@ -1,1 +1,14 @@
 //* Init Interfaces
+
+interface IClock {
+  time: Date
+  setTime(date: Date): void
+}
+
+class Clock implements IClock {
+  time: Date = new Date()
+
+  setTime(date: Date): void {
+    this.time = date
+  }
+}

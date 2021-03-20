494f4dbd439c58a9b5a3787ec702d510c933e09a
DeAdFrOnt <semenukmaks@gmail.com>
2021-03-20 13:34:06 +0200

    basic type

--- TypeScript/types.ts
+++ TypeScript/types.ts
@@ -1,1 +1,20 @@
 //* Init Types
+
+const isFetching: boolean = true
+const isLoading: boolean = false
+
+const int: number = 42
+const float: number = 4.2
+const num: number = 3e10
+
+const message: string = 'Hello Typescript'
+const numberArray: number[] = [1, 1, 2, 3, 5, 8, 13]
+const numberArray2: Array<number> = [1, 1, 2, 3, 5, 8, 13]
+
+const words: string[] = ['Hello', 'Typescript']
+
+const contact: [string, number] = ['Maksym', 324543565]
+
+let changVariable: any = 42
+changVariable = 'New String'
+changVariable = []

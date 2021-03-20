de6e8266a2e316a00061e87fed1519b7556dad23
DeAdFrOnt <semenukmaks@gmail.com>
2021-03-20 14:53:55 +0200

    enum

--- TypeScript/enum.ts
+++ TypeScript/enum.ts
@@ -1,1 +1,22 @@
 //* Init Enum
+
+enum Membership {
+  Simple,
+  Standard,
+  Premium,
+}
+
+const membership = Membership.Standard
+const membershipReverse = Membership[2]
+
+console.log(membership)
+console.log(membershipReverse)
+
+enum SocialMedia {
+  VK = 'VK',
+  FACEBOOK = 'FACEBOOK',
+  INSTAGARAM = 'INSTAGRAM',
+}
+
+const social = SocialMedia.INSTAGARAM
+console.log(social)

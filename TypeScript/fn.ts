f110f21622fd26d93404fe5a97b768dbddebd925
DeAdFrOnt <semenukmaks@gmail.com>
2021-03-20 15:10:34 +0200

    overload

--- TypeScript/fn.ts
+++ TypeScript/fn.ts
@@ -1,1 +1,25 @@
 //* Init Functions
+
+interface MyPositionWithDefault extends MyPosition {
+  default: string
+}
+
+function position(): MyPosition
+function position(a: number): MyPositionWithDefault
+function position(a: number, b: number): MyPosition
+
+function position(a?: number, b?: number) {
+  if (!a && !b) {
+    return { x: undefined, y: undefined }
+  }
+
+  if (a && !b) {
+    return { x: a, y: undefined, default: a.toString() }
+  }
+
+  return { x: a, y: b }
+}
+
+console.log('Empty: ', position())
+console.log('One param: ', position(42))
+console.log('Two params: ', position(15, 11))

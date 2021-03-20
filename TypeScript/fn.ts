8c8266fd8abbfd620388b9a25cfaf12da2d8ee3c
DeAdFrOnt <semenukmaks@gmail.com>
2021-03-20 15:09:24 +0200

    fn

--- TypeScript/fn.ts
+++ TypeScript/fn.ts
@@ -1,1 +1,14 @@
 //* Init Functions
+
+function add(a: number, b: number): number {
+  return a + b
+}
+
+function toUpperCase(str: string): string {
+  return str.trim().toUpperCase()
+}
+
+interface MyPosition {
+  x: number | undefined
+  y: number | undefined
+}

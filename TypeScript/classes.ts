b5c0524f2d7a3f8a6ee1de0546b139869097b74e
DeAdFrOnt <semenukmaks@gmail.com>
2021-03-20 15:18:47 +0200

    classes

--- TypeScript/classes.ts
+++ TypeScript/classes.ts
@@ -1,1 +1,28 @@
 //* Init Classes
+
+class Typescript {
+  version: string
+
+  constructor(version: string) {
+    this.version = version
+  }
+
+  info(name: string) {
+    return `[${name}]: Typescript version is ${this.version}`
+  }
+}
+
+// class Car {
+//   readonly model: string
+//   readonly numberOfWheels: number = 4
+
+//   constructor(theModel: string) {
+//     this.model = theModel
+//   }
+// }
+
+//* same as above
+class Car {
+  readonly numberOfWheels: number = 4
+  constructor(readonly model: string) {}
+}

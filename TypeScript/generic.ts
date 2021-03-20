bfd7145df863bd2b65e115ace4440b51feb8afae
DeAdFrOnt <semenukmaks@gmail.com>
2021-03-20 16:04:38 +0200

    generics

--- TypeScript/generic.ts
+++ TypeScript/generic.ts
@@ -1,1 +1,11 @@
 //* Init Generics
+
+const arrayOfNumbers: Array<number> = [1, 1, 2, 3, 5]
+const arrayOfStrings: Array<string> = ['Hello', 'Maksym']
+
+function reverse<T>(array: T[]): T[] {
+  return array.reverse()
+}
+
+reverse(arrayOfNumbers)
+reverse(arrayOfStrings)

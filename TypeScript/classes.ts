92211e180ecae6c0266919aa946a429ca69fa83f
DeAdFrOnt <semenukmaks@gmail.com>
2021-03-20 15:29:57 +0200

    abstract

--- TypeScript/classes.ts
+++ TypeScript/classes.ts
@@ -1,1 +1,16 @@
 //* Init Classes
+
+abstract class Component {
+  abstract render(): void
+  abstract info(): string
+}
+
+class AppComponent extends Component {
+  render(): void {
+    console.log('Component on render')
+  }
+
+  info(): string {
+    return 'This is info'
+  }
+}

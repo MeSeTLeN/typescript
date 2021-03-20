053f3066794c14a630f7e7217149b5f5e80c3580
DeAdFrOnt <semenukmaks@gmail.com>
2021-03-20 13:34:56 +0200

    fn type

--- TypeScript/types.ts
+++ TypeScript/types.ts
@@ -1,1 +1,14 @@
 //* Init Types
+
+function sayMyName(name: string): void {
+  console.log(name)
+}
+sayMyName('Maksym')
+
+function throwError(message: string): never {
+  throw new Error(message)
+}
+
+function infinite(): never {
+  while (true) {}
+}

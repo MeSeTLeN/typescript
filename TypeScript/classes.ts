185c730c2249546710487099efe42104b091ab18
DeAdFrOnt <semenukmaks@gmail.com>
2021-03-20 15:25:15 +0200

    modification

--- TypeScript/classes.ts
+++ TypeScript/classes.ts
@@ -1,1 +1,26 @@
 //* Init Classes
+
+class Animal {
+  protected voice: string = ''
+  public color: string = 'black'
+
+  constructor() {
+    this.go()
+  }
+
+  private go() {
+    console.log('Go')
+  }
+}
+
+class Cat extends Animal {
+  protected voice: string = ''
+  public setVoice(voice: string): void {
+    this.voice = voice
+  }
+}
+
+const cat = new Cat()
+cat.setVoice('test')
+console.log(cat.color)
+//! cat.voice !!cause protected
